4688b849ffb3cbcdfce0ac11b22efd70dcbf160d
iliasPs <ilias.paraskevop@gmail.com>
2018-06-05 20:06:59 +0300

    -

--- app/src/main/java/com/example/android/mynewsstand/EconomyFragment.java
+++ app/src/main/java/com/example/android/mynewsstand/EconomyFragment.java
@@ -3,13 +3,12 @@ package com.example.android.mynewsstand;
 import android.app.LoaderManager;
 import android.content.Loader;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,38 +19,31 @@ public class EconomyFragment extends Fragment implements LoaderManager.LoaderCal
     private static final String GUARDIAN_QUERY = "https://content.guardianapis.com/search?q=sports&show-elements=image&api-key=test&show-fields=thumbnail&show-tags=contributor&order-by=newest";
     public static final String LOG_TAG = SportsFragment.class.getName();
 
-    public EconomyFragment() {
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.news_item_list, container, false);
 
+        getActivity().getLoaderManager().initLoader(0, null, this).forceLoad();
 
         final ArrayList<News> news = new ArrayList<News>();
+
 
         mAdapter = new NewsListsAdapter(getActivity(), news);
         ListView listView = (ListView) rootView.findViewById(R.id.list);
         listView.setAdapter(mAdapter);
-
-
         return rootView;
-    }
+       }
 
     @Override
     public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
         // Create a new loader for the given URL
         Log.i(LOG_TAG, "test: onCreateLoader() called...");
         return new NewsLoader(getActivity().getApplication(), GUARDIAN_QUERY);
-
     }
     @Override
     public void onLoadFinished(Loader<List<News>> loader, List<News> news) {
-
-
         // Clear the adapter of previous earthquake data
         Log.i(LOG_TAG, "test onLoadFinished() called...");
         mAdapter.clear();
@@ -66,13 +58,12 @@ public class EconomyFragment extends Fragment implements LoaderManager.LoaderCal
         // Clear the adapter of previous earthquake data
         //mAdapter.clear();
 
-        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+        // If there is a valid list of {@link News}s, then add them to the adapter's
         // data set. This will trigger the ListView to update.
         if (news != null && !news.isEmpty()) {
             mAdapter.addAll(news);
         }
     }
-
     @Override
     public void onLoaderReset(Loader<List<News>> loader) {
         // Loader reset, so we can clear out our existing data.
